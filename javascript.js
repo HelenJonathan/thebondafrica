93ed0f5152fd6a20a906ca0902abecf23b14d723
Helen Efebe <efebehelen95@gmail.com>
2023-05-08 17:11:58 +0100

    team page added

---
+++ javascript.js
@@ -0,0 +1,75 @@
+// navbar styles
+
+const headerEl = document.querySelector('.navbar')
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 400) {
+    headerEl.classList.add('navbar-scrolled');
+  }else if (window.scrollY <= 400) {
+    headerEl.classList.remove('navbar-scrolled');
+  }
+})
+
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
+
+
+
+// Slider logic
+
+let slideIndex = 1;
+showSlides();
+
+function plusSlides(n) {
+  changeSlide(slideIndex += n);
+}
+
+function currentSlide(n) {
+  changeSlide(slideIndex = n);
+}
+
+function changeSlide(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+
